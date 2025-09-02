1b2c5240a7df50bcb614dfc9ce4e86eea6d513a9
Paul Diepold <paul.diepold@outlook.de>
2025-09-02 16:01:25 +0200

    extract layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from '#ui/types'
+
+const { loggedIn, user, clear } = useUserSession()
+const colorMode = useColorMode()
+
+const isDarkMode = computed({
+  get: () => colorMode.preference === 'dark',
+  set: () =>
+    (colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark')
+})
+const items = [
+  [
+    {
+      label: 'Logout',
+      icon: 'i-lucide-log-out',
+      onSelect: clear
+    }
+  ]
+] satisfies DropdownMenuItem[][]
+</script>
+
+<template>
+  <div>
+    <UContainer class="min-h-screen flex flex-col my-4 print:hidden">
+      <div class="mb-2 text-right">
+        <UButton
+          square
+          variant="ghost"
+          color="neutral"
+          :icon="
+            $colorMode.preference === 'dark' || $colorMode.preference === 'system'
+              ? 'i-lucide-moon'
+              : 'i-lucide-sun'
+          "
+          @click="isDarkMode = !isDarkMode"
+        />
+      </div>
+
+      <UCard variant="subtle">
+        <template #header>
+          <h3 class="text-lg font-semibold leading-6">
+            <NuxtLink to="/">
+              Pralinen
+            </NuxtLink>
+          </h3>
+          <UButton
+            v-if="!loggedIn"
+            to="/api/auth/github"
+            icon="i-simple-icons-github"
+            label="Login with GitHub"
+            color="neutral"
+            size="xs"
+            external
+          />
+          <div
+            v-else
+            class="flex flex-wrap -mx-2 sm:mx-0"
+          >
+            <UButton
+              :to="`/admin/years/${new Date().getFullYear()}`"
+              icon="i-lucide-settings"
+              label="Administration"
+              :color="$route.path.includes('/admin/years') ? 'primary' : 'neutral'"
+              variant="ghost"
+            />
+            <UDropdownMenu
+              v-if="user"
+              :items="items"
+            >
+              <UButton
+                color="neutral"
+                variant="ghost"
+                trailing-icon="i-lucide-chevron-down"
+              >
+                <UAvatar
+                  :src="`https://github.com/${user.login}.png`"
+                  :alt="user.login"
+                  size="3xs"
+                />
+                {{ user.login }}
+              </UButton>
+            </UDropdownMenu>
+          </div>
+        </template>
+        <slot />
+      </UCard>
+
+      <footer class="text-center mt-2">
+        <NuxtLink
+          href="https://github.com/pauldiepold/praline-poll"
+          target="_blank"
+          class="text-sm text-neutral-500 hover:text-neutral-700"
+        >
+          GitHub
+        </NuxtLink>
+      </footer>
+    </UContainer>
+  </div>
+</template>
